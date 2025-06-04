eef640a321e22dc369cef7e781b5be4a09e76cd5
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-06-04 19:14:06 +0700

    fix: missing spacing

--- src/app/_components/channel/main-section.tsx
+++ src/app/_components/channel/main-section.tsx
@@ -29,7 +29,7 @@ export const ChannelMainSection: React.ComponentType<{
   return (
     <main className="flex h-screen flex-1 flex-col">
       <header className="flex shrink-0 items-center gap-2 border-b py-2">
-        <div className="flex items-center gap-2 px-4">
+        <div className="flex items-center gap-2 px-4 py-2">
           <div className="flex items-center gap-2">
             <SidebarTrigger />
             <div className="h-4">
